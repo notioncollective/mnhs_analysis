0840cf72541fa29b52db563e9c7763eb81b91348
Andy Dayton <amdayton@gmail.com>
2014-07-04 14:18:23 -0400

    Updated wordcount_byissue script so that it doesn't include issues w/out ocr

--- scripts/analytics/wordcount_byissue.js
+++ scripts/analytics/wordcount_byissue.js
@@ -28,7 +28,7 @@ MongoClient.connect('mongodb://127.0.0.1:27017/mnhs', function(err, db) {
             query: {},
 
     		// The scope provides variables to the map/reduce functions
-    		scope: {regex: regex},
+    		scope: {regex: regex, OCR_EMPTY_STR: 'no_text'},
             
             // sort the INPUT records
             // sort: {},
@@ -50,14 +50,18 @@ function map() {
 	// console.log('map!');
     var pages = this.pages,
         numPages = pages.length,
-        totalOccurances = 0;
+        totalOccurances = 0,
+        hasOcrContent = false;
 
     for (var i = 0; i < numPages; i++) {
         // get ocr content of the current document
         var ocr = pages[i].ocr;
 
         // if ocr doesn't exist, go to next page
-        if (!ocr || !ocr.match) continue;
+        if (!ocr || !ocr.match || ocr === OCR_EMPTY_STR) continue;
+
+        // this one has some ocr content
+        hasOcrContent = true;
 
         // find all matchs of searchWord in ocr
         var matches = ocr.match(this['regex']);
@@ -75,7 +79,9 @@ function map() {
     // if (totalOccurances == 0) return;
 
     // for each newspaper edition, emit the total occurances of the word:
-    emit(this._id, { occurances: totalOccurances, group: 1});
+    if(hasOcrContent) {
+        emit(this._id, { occurances: totalOccurances, group: 1});
+    }
 }
 
 function reduce(key, values) {
